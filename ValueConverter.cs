7482ca518f299a5e007c41b9c76ac6f87359f796
vijaybiraman <109381804+vijaybiraman@users.noreply.github.com>
2022-08-17 09:06:40 +0530

    Add files via upload

---
+++ ValueConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CodeFirstAssignment
+{
+    public class ValueConverter :IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            decimal NetStock = 0;
+            if (values[0] != null && values[1]!=null)
+            {
+            
+                decimal NetStock1 = (decimal)(values[0]);
+              short  NetStock2 = (short)values[1];
+                NetStock = NetStock1 * NetStock2;
+                return NetStock.ToString(parameter as string, culture);
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
